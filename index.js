4f5a8da80d1e8dd02f626744b4b9f3babf7ca954
davidkoojohn <davidkoojohn@gmail.com>
2018-03-15 15:02:39 +0800

    update index

--- index.js
+++ index.js
@@ -8,43 +8,31 @@ const cheerio = require('cheerio')
 const app = express()
 
 app.get('/', (req, res) => {
-    // const q = req.query.q
-    // const md5Value = utility.md5(q)
-    // res.send(md5Value)
     superagent.get('http://m.zgmht.com/company.html')
         .charset('gbk')
         .end((err, sres) => {
-    // superagent.get('https://cnodejs.org/').end((err, sres) => {
-        if (err) {
-            return next(err)
-        }
 
-        const $ = cheerio.load(sres.text, {
-            decodeEntities: false
-        })
-        let items = []
-        /*$('#topic_list .topic_title').each(function (idx, element) {
-            const $element = $(element);
-            console.log(idx)
-            items.push({
-                title: $element.attr('title'),
-                href: $element.attr('href')
+            if (err) {
+                return next(err)
+            }
+            const $ = cheerio.load(sres.text, {
+                decodeEntities: false
             })
-        })*/
-        $('#moreList li').each(function (idx, element) {
-            const $tit1 = $('#moreList li .tit1').eq(idx);
-            const $contact_person = $('#moreList li .contact_person').eq(idx);
-            const $tel = $('#moreList li .tel').eq(idx);
-            items.push({
-                cName: $tit1.text(),
-                name: $contact_person.text(),
-                tel: $tel.text()
-            })
-        })
+            let items = []
 
-        res.send(items)
-        // console.log(sres.text)
-    })
+            $('#moreList li').each(function (idx, element) {
+                const $tit1 = $('#moreList li .tit1').eq(idx);
+                const $contact_person = $('#moreList li .contact_person').eq(idx);
+                const $tel = $('#moreList li .tel').eq(idx);
+                items.push({
+                    cName: $tit1.text(),
+                    name: $contact_person.text(),
+                    tel: $tel.text()
+                })
+            })
+
+            res.send(items)
+        })
 })
 
 app.listen(7777, () => {
